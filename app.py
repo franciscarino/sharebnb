faabe94332b90eb01670e94898407382beee32c9
Francis Carino <fgcarino@gmail.com>
2022-10-27 20:54:16 -0700

    preparing to deploy to heroku

--- app.py
+++ app.py
@@ -15,12 +15,13 @@ load_dotenv()
 
 CURR_USER_KEY = "curr_user"
 
-database_url = os.environ['DATABASE_URL']
-database_url = database_url.replace('postgres://', 'postgresql://')
+# database_url = os.environ['DATABASE_URL']
+# database_url = database_url.replace('postgres://', 'postgresql://')
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///sharebnb'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
