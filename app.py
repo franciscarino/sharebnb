2ecacdda5fe899f36c89d286bc6c96346b1af54b
Francis Carino <fgcarino@gmail.com>
2022-11-02 11:46:33 -0700

    update db url

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 CURR_USER_KEY = "curr_user"
 
-database_url = os.environ['DATABASE_URL1']
+database_url = os.environ['DATABASE_URL']
 database_url = database_url.replace('postgres://', 'postgresql://')
 
 app = Flask(__name__)
